09c7e3b5c7920e7629ad21c4085a64128de38846
Kamil Baldyga <baldygakamil@gmail.com>
2025-05-20 16:50:50 +0200

    step2 header & cards almost 100% stylized

--- src/components/step2/Step2.jsx
+++ src/components/step2/Step2.jsx
@@ -9,19 +9,21 @@ export default function Step2() {
 
   return (
     <>
-      <Header h1={headerH1} span={headerSpan} />
+      <div className="step2-container">
+        <Header h1={headerH1} span={headerSpan} />
 
-      <div className="plan-cards">
-        {data.map((plan, index) => (
-          <Card
-            key={index}
-            planTitle={plan.title}
-            planPrice={plan.price.monthly}
-            paymentFrequency={"mo"}
-            imgSrc={plan.image}
-            altText={`image of the ${plan.title} plan`}
-          />
-        ))}
+        <div className="plan-cards">
+          {data.map((plan, index) => (
+            <Card
+              key={index}
+              planTitle={plan.title}
+              planPrice={plan.price.monthly}
+              paymentFrequency={"mo"}
+              imgSrc={plan.image}
+              altText={`image of the ${plan.title} plan`}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
