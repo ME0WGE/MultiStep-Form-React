4beede4149d2fd783eb1bb88f30e840e91ff5907
Kamil Baldyga <baldygakamil@gmail.com>
2025-05-21 11:16:07 +0200

    step2 sylized

--- src/components/step2/Step2.jsx
+++ src/components/step2/Step2.jsx
@@ -9,30 +9,40 @@ export default function Step2() {
   const headerSpan = "You have the option of monthly or yearly billing";
 
   return (
-    <>
-      <div className="step2">
-        <div className="menu-container">
-          {/* Menu */}
-          <Sidebar activeStep={2} />
-        </div>
-        {/* Content */}
-        <div className="step2-container">
-          <Header h1={headerH1} span={headerSpan} />
+    <div className="step2">
+      <div className="menu-container">
+        <Sidebar activeStep={2} />
+      </div>
+      <div className="step2-container">
+        <Header h1={headerH1} span={headerSpan} />
 
-          <div className="plan-cards">
-            {data.map((plan, index) => (
-              <Card
-                key={index}
-                planTitle={plan.title}
-                planPrice={plan.price.monthly}
-                paymentFrequency={"mo"}
-                imgSrc={plan.image}
-                altText={`image of the ${plan.title} plan`}
-              />
-            ))}
-          </div>
+        <div className="plan-cards">
+          {data.map((plan, index) => (
+            <Card
+              key={index}
+              planTitle={plan.title}
+              planPrice={plan.price.monthly}
+              paymentFrequency={"mo"}
+              imgSrc={plan.image}
+              altText={`image of the ${plan.title} plan`}
+            />
+          ))}
+        </div>
+
+        <div className="billing-switch">
+          <span className={"monthly"}>Monthly</span>
+          <label className="switch">
+            <input type="checkbox" checked={"yearly"} />
+            <span className="slider"></span>
+          </label>
+          <span className={"yearly"}>Yearly</span>
+        </div>
+
+        <div className="step2-buttons">
+          <button className="go-back">Go Back</button>
+          <button className="next-step">Next Step</button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
