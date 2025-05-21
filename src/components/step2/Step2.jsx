58b1aa4ef38b454deb63529c676e78fda22402b2
Kamil Baldyga <baldygakamil@gmail.com>
2025-05-21 09:55:29 +0200

    add menu

--- src/components/step2/Step2.jsx
+++ src/components/step2/Step2.jsx
@@ -2,6 +2,7 @@ import Card from "../card/Card";
 import Header from "../header/Header";
 import "./step2.css";
 import data from "../../data/data.json";
+import Menu from "../menu/Menu";
 
 export default function Step2() {
   const headerH1 = "Select Your Plan";
@@ -9,20 +10,27 @@ export default function Step2() {
 
   return (
     <>
-      <div className="step2-container">
-        <Header h1={headerH1} span={headerSpan} />
+      <div className="step2">
+        <div className="menu-container">
+          {/* Menu */}
+          <Menu />
+        </div>
+        {/* Content */}
+        <div className="step2-container">
+          <Header h1={headerH1} span={headerSpan} />
 
-        <div className="plan-cards">
-          {data.map((plan, index) => (
-            <Card
-              key={index}
-              planTitle={plan.title}
-              planPrice={plan.price.monthly}
-              paymentFrequency={"mo"}
-              imgSrc={plan.image}
-              altText={`image of the ${plan.title} plan`}
-            />
-          ))}
+          <div className="plan-cards">
+            {data.map((plan, index) => (
+              <Card
+                key={index}
+                planTitle={plan.title}
+                planPrice={plan.price.monthly}
+                paymentFrequency={"mo"}
+                imgSrc={plan.image}
+                altText={`image of the ${plan.title} plan`}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </>
